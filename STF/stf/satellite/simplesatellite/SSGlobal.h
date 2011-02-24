21c4a6e7652fd4771f0aaef72bc37cabb3cde81c
Taiga Noumi <Noumi.Taiga@gmail.com>
2011-02-24 20:00:42 +0900

    remove trivial cpp files

--- STF/stf/satellite/simplesatellite/SSGlobal.h
+++ STF/stf/satellite/simplesatellite/SSGlobal.h
@@ -8,6 +8,10 @@
 #ifndef satellite_simplesatellite_SSGlobal_h
 #define satellite_simplesatellite_SSGlobal_h
 #include "../../GlobalObject.h"
+#include "../../core/manager/Includes.h"
+#include "../../core/devicedriver/Includes.h"
+#include "../../datatype/SatelliteModel.h"
+
 
 namespace stf {
 namespace factory {
@@ -16,23 +20,28 @@ template <class Env> class SSFactory;
 
 template <class Env>
 struct SSGlobal : public Global<Env>{
-	/*
+	
 	virtual const datatype::Time get_global_time(){
-		return this->pr_clock->get_time();
+		return this->ss_clock->get_time();
 	}
 	virtual const datatype::DateTime get_global_datetime(){
-		return this->pr_clock->get_datetime();
+		return this->ss_clock->get_datetime();
 	}
 	virtual const datatype::SatelliteModel get_satellitemodel() const{
-		return this->prism_body_;
+		return this->ss_body_;
 	}
 	virtual const core::datapool::AocsDataPool* get_datapool() const {
-		return this->pr_aocsdatapool;
+		return this->ss_aocsdatapool;
 	}
 	virtual const core::datapool::EventDataPool* get_eventdatapool() const {
-		return this->pr_eventdatapool;
-	}*/
+		return this->ss_eventdatapool;
+	}
 
+	datapool::AocsDataPool* ss_aocsdatapool;
+	datapool::EventDataPool* ss_eventdatapool;
+
+	devicedriver::clock::DummyClock* ss_clock;
+	datatype::SatelliteModel ss_body_;
 };
 
 } /* End of namespace stf */
